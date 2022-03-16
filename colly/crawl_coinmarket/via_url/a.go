28a261f409c0dfbc1c5319c73c6d0c726d84be41
w0x7ce <tian.r.king@gmail.com>
2022-03-16 23:43:35 +0800

    fix golang crawl

--- colly/crawl_coinmarket/via_url/a.go
+++ colly/crawl_coinmarket/via_url/a.go
@@ -48,7 +48,7 @@ func main() {
 		r.Headers.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
 		r.Headers.Set("Sec-Fetch-Site", "same-origin")
 		r.Headers.Set("Sec-Fetch-Mode", "navigate")
-		r.Headers.Set("Sec-Fetch-User", "?1")
+		r.Headers.Set("Sec-Fetch-User", "?2")
 		r.Headers.Set("Sec-Fetch-Dest", "document")
 		r.Headers.Set("Referer", "https://coinmarketcap.com/all/views/all/")
 		r.Headers.Set("Accept-Language", "zh-TW,zh-HK;q=0.9,zh;q=0.8,en;q=0.7,zh-CN;q=0.6,en-GB;q=0.5,en-US;q=0.4")
@@ -117,6 +117,24 @@ func main() {
 //   -H 'if-modified-since: Wed, 16 Mar 2022 06:50:24 GMT' \
 //   --compressed
 ///  数据源
+
+// curl 'https://api.coinmarketcap.com/data-api/v3/topsearch/rank' \
+//   -H 'authority: api.coinmarketcap.com' \
+//   -H 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"' \
+//   -H 'accept: application/json, text/plain, */*' \
+//   -H 'platform: web' \
+//   -H 'sec-ch-ua-mobile: ?0' \
+//   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36' \
+//   -H 'x-request-id: 62f24a92-35f9-4cda-be48-3fe381aa1b27' \
+//   -H 'sec-ch-ua-platform: "Windows"' \
+//   -H 'origin: https://coinmarketcap.com' \
+//   -H 'sec-fetch-site: same-site' \
+//   -H 'sec-fetch-mode: cors' \
+//   -H 'sec-fetch-dest: empty' \
+//   -H 'referer: https://coinmarketcap.com/' \
+//   -H 'accept-language: zh-TW,zh-HK;q=0.9,zh;q=0.8,en;q=0.7,zh-CN;q=0.6,en-GB;q=0.5,en-US;q=0.4' \
+//   -H 'if-modified-since: Wed, 16 Mar 2022 15:34:16 GMT' \
+//   --compressed
 
 // curl 'https://api.coinmarketcap.com/data-api/v3/map/all?listing_status=active,untracked&exchangeAux=is_active,status&cryptoAux=is_active,status&start=10001&limit=10000' \
 //   -H 'authority: api.coinmarketcap.com' \
