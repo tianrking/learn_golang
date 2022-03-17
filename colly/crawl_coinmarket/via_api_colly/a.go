7eddf68bb2a1993c1b7dedc351f52f0311d39b7a
w0x7ce <tian.r.king@gmail.com>
2022-03-17 10:18:14 +0800

    add table

--- colly/crawl_coinmarket/via_api_colly/a.go
+++ colly/crawl_coinmarket/via_api_colly/a.go
@@ -9,19 +9,6 @@ import (
 )
 
 func main() {
-	// fName := "cryptocoinmarketcap.csv"
-
-	// file, err := os.Create(fName)
-	// if err != nil {
-	// 	log.Fatalf("Cannot create file %q: %s\n", fName, err)
-	// 	return
-	// }
-	// defer file.Close()
-	// writer := csv.NewWriter(file)
-	// defer writer.Flush()
-
-	// Write CSV header
-	// writer.Write([]string{"Name", "Symbol", "Price (USD)", "Volume (USD)", "Market capacity (USD)", "Change (1h)", "Change (24h)", "Change (7d)"})
 
 	// Instantiate default collector
 	c := colly.NewCollector()
@@ -38,43 +25,34 @@ func main() {
 	c.OnRequest(func(r *colly.Request) { // 需要寫這一段 User-Agent 降低被ban 可能性
 		//r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36")
 
-		r.Headers.Set("Authority", "coinmarketcap.com")
-		r.Headers.Set("Cache-Control", "max-age=0")
-		r.Headers.Set("Sec-Ch-Ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Microsoft Edge\";v=\"99\"")
-		r.Headers.Set("Sec-Ch-Ua-Mobile", "?0")
-		r.Headers.Set("Sec-Ch-Ua-Platform", "\"Windows\"")
-		r.Headers.Set("Upgrade-Insecure-Requests", "1")
-		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39")
-		r.Headers.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-		r.Headers.Set("Sec-Fetch-Site", "same-origin")
-		r.Headers.Set("Sec-Fetch-Mode", "navigate")
-		r.Headers.Set("Sec-Fetch-User", "?2")
-		r.Headers.Set("Sec-Fetch-Dest", "document")
-		r.Headers.Set("Referer", "https://coinmarketcap.com/all/views/all/")
-		r.Headers.Set("Accept-Language", "zh-TW,zh-HK;q=0.9,zh;q=0.8,en;q=0.7,zh-CN;q=0.6,en-GB;q=0.5,en-US;q=0.4")
+		r.Headers.Set("authority", "api.coinmarketcap.com")
+		// r.Headers.Set("sec-ch-ua", "" Not A;Brand";v="99", "Chromium";v="99", "Microsoft Edge";v="99"")
+		r.Headers.Set("accept", "application/json, text/plain, */*")
+		r.Headers.Set("platform", "web")
+		r.Headers.Set("sec-ch-ua-mobile", "?1")
+		r.Headers.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39")
+		r.Headers.Set("x-request-id", "62f24a92-35f9-4cda-be48-3fe381aa1b27") //62f24a92-35f9-4cda-be48-3fe381aa1b27 "94ae8373-239b-4296-a353-15536d721619"
+		r.Headers.Set("sec-ch-ua-platform", "Windows")
+		r.Headers.Set("origin", "https://coinmarketcap.com")
+		r.Headers.Set("sec-fetch-site", "same-site")
+		r.Headers.Set("sec-fetch-mode", "cors")
+		r.Headers.Set("sec-fetch-dest", "empty")
+		r.Headers.Set("referer", "https://coinmarketcap.com/")
+		r.Headers.Set("accept-language", "zh-TW,zh-HK;q=0.9,zh;q=0.8,en;q=0.7,zh-CN;q=0.6,en-GB;q=0.5,en-US;q=0.4")
 
 	})
 
 	c.OnResponse(func(r *colly.Response) { // 當Visit訪問網頁後，網頁響應(Response)時候執行的事情
-		// fmt.Println(string(r.Body)) // 返回的Response物件r.Body 是[]Byte格式，要再轉成字串
+		fmt.Println(string(r.Body)) // 返回的Response物件r.Body 是[]Byte格式，要再轉成字串
 		// fmt.Println(reflect.TypeOf(r))
 	})
 
 	c.OnHTML("tr[class='cmc-table-row']", func(e *colly.HTMLElement) {
-		// writer.Write([]string{
-		// 	e.ChildText(".currency-name-container"),
-		// 	e.ChildText(".col-symbol"),
-		// 	e.ChildAttr("a.price", "data-usd"),
-		// 	e.ChildAttr("a.volume", "data-usd"),
-		// 	e.ChildAttr(".market-cap", "data-usd"),
-		// 	e.ChildText(".percent-1h"),
-		// 	e.ChildText(".percent-24h"),
-		// 	e.ChildText(".percent-7d"),
-		// })
-		fmt.Println(e.ChildText("a[class='cmc-table__column-name--name cmc-link']"))
+		// fmt.Println(e.ChildText("a[class='cmc-table__column-name--name cmc-link']"))
 	})
 
-	c.Visit("https://coinmarketcap.com/all/views/all/")
+	c.Visit("https://api.coinmarketcap.com/data-api/v3/topsearch/rank")
+	// c.Visit("https://coinmarketcap.com/all/views/all/")
 	// c.Visit("https://coinmarketcap.com/currencies/bitcoin/")
 
 	// log.Printf("Scraping finished, check file %q for results\n", fName)
@@ -134,6 +112,24 @@ func main() {
 //   -H 'referer: https://coinmarketcap.com/' \
 //   -H 'accept-language: zh-TW,zh-HK;q=0.9,zh;q=0.8,en;q=0.7,zh-CN;q=0.6,en-GB;q=0.5,en-US;q=0.4' \
 //   -H 'if-modified-since: Wed, 16 Mar 2022 15:34:16 GMT' \
+//   --compressed
+
+// curl 'https://api.coinmarketcap.com/data-api/v3/topsearch/rank' \
+//   -H 'authority: api.coinmarketcap.com' \
+//   -H 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="99", "Microsoft Edge";v="99"' \
+//   -H 'accept: application/json, text/plain, */*' \
+//   -H 'platform: web' \
+//   -H 'sec-ch-ua-mobile: ?0' \
+//   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39' \
+//   -H 'x-request-id: 8a2c10aa-4c23-455f-b6fe-80109fcdbd48' \
+//   -H 'sec-ch-ua-platform: "Windows"' \
+//   -H 'origin: https://coinmarketcap.com' \
+//   -H 'sec-fetch-site: same-site' \
+//   -H 'sec-fetch-mode: cors' \
+//   -H 'sec-fetch-dest: empty' \
+//   -H 'referer: https://coinmarketcap.com/' \
+//   -H 'accept-language: zh-TW,zh-HK;q=0.9,zh;q=0.8,en;q=0.7,zh-CN;q=0.6,en-GB;q=0.5,en-US;q=0.4' \
+//   -H 'if-modified-since: Thu, 17 Mar 2022 01:38:57 GMT' \
 //   --compressed
 
 // curl 'https://api.coinmarketcap.com/data-api/v3/map/all?listing_status=active,untracked&exchangeAux=is_active,status&cryptoAux=is_active,status&start=10001&limit=10000' \
